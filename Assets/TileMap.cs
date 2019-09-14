8ec83a1b8a6f5f9a466e7b3b876778aa54d7b400
Austin <austin.forsling@gmail.com>
2019-09-13 20:03:00 -0600

    Added basic initialization of tile map and added comments.

--- Assets/TileMap.cs
+++ Assets/TileMap.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class TileMap : MonoBehaviour {
 
+    //Array of the different tile types, for easy reference using ints in tiles array.
     TileType[] tileTypes;
 
+    //2d array of ints that will be used to reference tileTypes and build a map
     int[,] tiles;
 
+    //Map sizes easily changed
     int mapSizeX = 10;
     int mapSizeY = 10;
 
     void Start()
     {
+        //Initialize tiles array.
         tiles = new int[mapSizeX, mapSizeY];
+
+        //Nested for loop fills out tiles array to build room
+        for(int i = 0; i < mapSizeX; i++)
+        {
+            for(int j = 0; j < mapSizeY; j++)
+            {
+                tiles[i, j] = 0;
+            }
+        }
     }
 
 }
